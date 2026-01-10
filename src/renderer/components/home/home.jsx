cd490cc8ba6d9860942f8c491bbf0e32ebb38228
Yogirajsinh <yogirajsinhparmar02@gmail.com>
2026-01-10 16:59:27 +0530

    fix: builder config

--- src/renderer/components/home/home.jsx
+++ src/renderer/components/home/home.jsx
@@ -74,7 +74,7 @@ export function HomePage() {
               className='flex items-center space-x-3 rtl:space-x-reverse'
             >
               <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-                Test
+                DocAlert
               </span>
             </Link>
 
